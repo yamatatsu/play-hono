1f427787e6e1602377611a0d5bbda47d5a99dc8e
yamatatsu <the.kinnikuman@gmail.com>
2024-09-17 00:00:43 +0900

    chore: use `showRoutes()`

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -1,6 +1,9 @@
 import { serve } from "@hono/node-server";
+import { showRoutes } from "hono/dev";
 import { app } from "./app";
 
-serve({ fetch: app.fetch, port: 3000 }, ({ port, address, family }) => {
+serve({ fetch: app.fetch, port: 3000 }, ({ port, address }) => {
 	console.info(`Server is running on port http://${address}:${port}`);
 });
+
+showRoutes(app);
