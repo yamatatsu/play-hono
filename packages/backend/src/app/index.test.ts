bbffc26e55c5dd1b70ac26af620798c06086d458
yamatatsu <the.kinnikuman@gmail.com>
2024-09-22 21:19:25 +0900

    test

--- packages/backend/src/app/index.test.ts
+++ packages/backend/src/app/index.test.ts
@@ -1,33 +1,43 @@
+import type { User } from "@prisma/client";
 import { testClient } from "hono/testing";
-import { expect, test } from "vitest";
+import { afterAll, expect, test } from "vitest";
+import { defineUserFactory, initialize } from "../__generated__/fabbrica";
+import { prisma } from "../adapters/db/client";
 import { app } from "./";
 
 const client = testClient(app);
 
-test("GET /users", async () => {
+initialize({ prisma });
+const UserFactory = defineUserFactory();
+
+const users: User[] = [];
+
+afterAll(async () => {
+	UserFactory.buildList;
+
+	await prisma.user.deleteMany({
+		where: { id: { in: users.map((u) => u.id) } },
+	});
+	await prisma.$disconnect();
+});
+
+test("GET /users/123", async () => {
+	// GIVEN
+	const user = await UserFactory.create();
+
 	// WHEN
-	const res = await client.users.$get();
+	const res = await client.users[":sub"].$get({ param: { sub: user.sub } });
 
 	// THEN
 	expect(res.status).toBe(200);
-	expect(await res.json()).toEqual([
-		{
-			id: "123",
-			name: "John Doe",
-			age: 42,
-		},
-	]);
+	expect(await res.json()).toEqual(user);
 });
 
 test("GET /users/123", async () => {
 	// WHEN
-	const res = await client.users[":id"].$get({ param: { id: "123" } });
+	const res = await client.users[":sub"].$get({ param: { sub: "test-sub" } });
 
 	// THEN
-	expect(res.status).toBe(200);
-	expect(await res.json()).toEqual({
-		id: "123",
-		name: "John Doe",
-		age: 42,
-	});
+	expect(res.status).toBe(404);
+	expect(await res.text()).toEqual("No user found");
 });
